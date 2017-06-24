f428f828359358c1a25d6116f93a8bd2f6641b47
bolando <biuro@solmedia.pl>
2017-06-24 15:50:06 +0200

    badges all

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,55 +3,47 @@
 defined('_JEXEC') or die; 
 ?>
  <table class="table table-hover table-condensed">
-    <!-- <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead> -->
-    <tbody>
       <tr>
         <td>Liczba artykułów: </td>
         <td><span class="badge badge-info hasTooltip" title="" data-original-title="Liczba artykułów"><?php echo $artykuly->a;?></span></td>
       </tr>
 	  <tr>
         <td>Liczba kategorii aktywnych: </td>
-        <td><?php echo $artykuly->b;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Liczba kategorii aktywnych"><?php echo $artykuly->b;?></span></td>
       </tr>
 	  <tr>
         <td>Liczba pozycji menu FrontEnd: </td>
-        <td><?php echo $menu[0]->a;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Liczba pozycji menu FrontEnd"><?php echo $menu[0]->a;?></span></td>
       </tr>
 	  <tr>
         <td>Liczba kategorii aktywnych: </td>
-        <td><?php echo $menu[0]->b;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Liczba kategorii aktywnych"><?php echo $menu[0]->b;?></span></td>
       </tr>
 	  <tr>
         <td>Liczba użytkowników: </td>
-        <td><?php echo $users;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Liczba użytkowników"><?php echo $users;?></span></td>
       </tr>
 	  <tr>
         <td>Posiada BetterPreview? </td>
-        <td><?php echo $betterpreview;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Posiada BetterPreview"><?php echo $betterpreview;?></span></td>
       </tr>
 	  <tr>
         <td>Posiada ImageSizer? </td>
-        <td><?php echo $imagesizer;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Posiada ImageSizer"><?php echo $imagesizer;?></span></td>
       </tr>
 	  <tr>
         <td>Posiada JCHOptimize? </td>
-        <td><?php echo $jchoptimize;?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="JCHOptimize?"><?php echo $jchoptimize;?></span></td>
       </tr>
 	  <?php if (class_exists('RSFirewallConfig')):?>
 	  <tr>
         <td>Wynik RSFirewall: </td>
-        <td><?php echo "$grade z $last";?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="RSFirewall"><?php echo "$grade z $last";?></span></td>
       </tr>
 	  <?php endif;?>
 	  <tr>
         <td>Wersja PHP: </td>
-        <td><?php echo phpversion();?></td>
+        <td><span class="badge badge-info hasTooltip" title="" data-original-title="Wersja PHP"><?php echo phpversion();?></span></td>
       </tr><tr>
         <td>Notatka: </td>
         <td><?php echo $notatka;?></td>
